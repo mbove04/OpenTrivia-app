a84957692b5a602de02752b71bc3b1a53c7c54b7
Martin Bove <fax_uu@hotmail.com>
2019-08-13 11:23:03 +0200

    Initial Commit

---
+++ OpenTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenTrivia
+//
+//  Created by Sailor on 13/08/2019.
+//  Copyright © 2019 sailor.OpenTrivia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
